be1c9953d397159f9a64a603c32a316b888c9000
Hardys <hardyscc@gmail.com>
2018-05-02 15:19:13 +0800

    -

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,40 +4,34 @@ import 'dom-testing-library/extend-expect'
 import { MockedProvider } from 'react-apollo/test-utils'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
-import { App, AppComponent, STOCKS_QUERY } from '../App'
+import { App, STOCKS_QUERY } from '../App'
 
-const data = {
-  stocks: [
-    { code: '0001.HK', name: '長和' },
-    { code: '0002.HK', name: '中電控股' }
-  ]
-}
-const mocks = [
-  {
-    request: { query: STOCKS_QUERY },
-    result: { data }
-  }
+const stocks = [
+  { code: '0001.HK', name: '長和' },
+  { code: '0002.HK', name: '中電控股' }
 ]
+const request = { query: STOCKS_QUERY }
+const result = { data: { stocks } }
+const error = new Error('something go wrong')
 
 describe('App', () => {
   it('render', async () => {
     const { container, getByText } = render(
-      <MockedProvider mocks={mocks}>
+      <MockedProvider mocks={[{ request, result }]}>
         <App />
       </MockedProvider>
     )
     await wait(() => getByText('Stocks'))
     expect(container).toMatchSnapshot()
   })
-})
 
-describe('AppComponent', () => {
-  it('handles a loading state', () => {
-    const { container } = render(<AppComponent loading />)
-    expect(container).toMatchSnapshot()
-  })
-  it('handles an error state', () => {
-    const { container } = render(<AppComponent error />)
+  it('error', async () => {
+    const { container, getByText } = render(
+      <MockedProvider mocks={[{ request, error }]}>
+        <App />
+      </MockedProvider>
+    )
+    await wait(() => getByText('Error'))
     expect(container).toMatchSnapshot()
   })
 })
